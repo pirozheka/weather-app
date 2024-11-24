495bfc2c54221e300c0d16e30772dd88c8e7766d
Jay <pirozheka@gmail.com>
2024-11-24 22:06:41 +0500

    start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "../styles/components/header.css";
 import { FaSearch } from "react-icons/fa";
 
-function Header() {
+const Header = () => {
     return (
-        <header className="header">
-            <div className="header__title">Simple Weather App</div>
-            <div className="header__city">
-                <div>Поиск по городу:</div>
-                <div className="header__city-searchbox">
-                    <input type="text" placeholder="Найти..." />
-                    <button type="submit"><FaSearch /></button>
+        <header>
+            <div className="container">
+                <div className="header">
+                    <div className="header__title">Simple Weather App</div>
+                    <div className="header__city">
+                        <div>Поиск по городу:</div>
+                        <div className="header__city-searchbox">
+                            <input type="text" placeholder="Найти..." />
+                            <button type="submit"><FaSearch /></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
